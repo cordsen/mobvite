a0bdf7b925e4b8720f56e23f78dfb04dfc572ef4
erik <cordsen@gmail.com>
2011-06-14 21:41:07 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem "rake", "0.8.7"
+gem 'rails', '3.0.7'
+gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+gem 'autotest-standalone'
+
+group :development do
+  gem 'rspec-rails', '2.5.0'
+  gem 'annotate-models', '1.0.4'
+end
+
+group :test do
+  gem 'rspec', '2.5.0'
+  gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobvite::Application.config.secret_token = '34790554dd335fb68e354e9df060116ba9b566b7b04810b70af3d3a037bf9e059c5a1456c1cd217990450ff8635b25d02e918b5a07a9547662084665b7e8f855'
